a4c50bfdd6b9b37e8d3df3fed5cafb471be62c1c
WhiteFangs <white-fangs@hotmail.fr>
2018-01-15 23:33:32 +0100

    Added simple Service Worker for offline mode with progressive web app

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+var log = console.log.bind(console);
+var version = "1.0.0";
+var cacheName = "fdc-sw";
+var cache = cacheName + "-" + version;
+var filesToCache = [
+'./manifest.json',
+'./js/vue.js',
+'./js/fieldDataCollector.js',
+'./css/style.css',
+'./index.html',
+];
+
+self.addEventListener("install", function(event) {
+    log('[ServiceWorker] Installing....');
+    event.waitUntil(caches
+        .open(cache)
+        .then(function(cache) {
+            log('[ServiceWorker] Caching files');
+            cache.addAll(filesToCache);
+        })
+        ); 
+});
+
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if(response) {
+                log("Fulfilling "+event.request.url+" from cache.");
+                return response;
+            } else {
+                log(event.request.url+" not found in cache fetching from network.");
+                return fetch(event.request);
+            }
+        })
+        );
+});
+
+self.addEventListener('activate', function(event) {
+  log('[ServiceWorker] Activate');
+  event.waitUntil(
+        caches.keys()
+        .then(function(keyList) {
+                Promise.all(keyList.map(function(key) {
+                    if (key !== cacheName) {
+                        log('[ServiceWorker] Removing old cache ', key);
+                            return caches.delete(key);
+                        }
+                    })
+                );
+            })
+        );
+});
